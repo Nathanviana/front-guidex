9d1f9df4898e1a3dd34f0df2a298570ecf58ee22
Nathanviana <viananathan03@gmail.com>
2025-06-19 14:06:56 -0300

    alteração na configuração do next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Desativa otimizações que usam o binding nativo do lightningcss
+  experimental: {
+    optimizeCss: false,
+    optimizeFonts: false,
+  },
+
+  // ...outras opções que você já tinha
 };
 
 export default nextConfig;
